e9882964a890734be8eb845d92d4950701f8474e
Spidy0408 <shashank.mishra0408@gmail.com>
2021-05-16 10:40:02 +0530

    inheritence

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var x;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,6 +16,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -35,6 +37,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    x= new Box(300,600,40,40);
 
 }
 
